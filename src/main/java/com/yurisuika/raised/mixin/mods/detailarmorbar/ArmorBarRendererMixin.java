d36b05dce7ef63f7b49cf2af3d1eded887a451e0
yurisuika <yurisuika@gmail.com>
2022-03-02 05:39:10 -0800

    Removed FAPI Dependency
    Updated to 1.18.2
    Removed Fabric API dependency
    Support for DetailArmorBar

---
+++ src/main/java/com/yurisuika/raised/mixin/mods/detailarmorbar/ArmorBarRendererMixin.java
@@ -0,0 +1,17 @@
+package com.yurisuika.raised.mixin.mods.detailarmorbar;
+
+import com.redlimerl.detailab.render.ArmorBarRenderer;
+import net.minecraft.client.util.Window;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ArmorBarRenderer.class)
+public class ArmorBarRendererMixin {
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "net/minecraft/client/util/Window.getScaledHeight()I"))
+    private int modifyAddonsScaledHeight(Window instance) {
+        return instance.getScaledHeight() - 1;
+    }
+
+}
