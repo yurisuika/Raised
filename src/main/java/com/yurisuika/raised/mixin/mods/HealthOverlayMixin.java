80d3426fcfb224fb6f7d61851ac34805d753f7de
yurisuika <yurisuika@protonmail.com>
2022-04-17 20:38:33 -0700

    Health Overlay support

---
+++ src/main/java/com/yurisuika/raised/mixin/mods/HealthOverlayMixin.java
@@ -0,0 +1,17 @@
+package com.yurisuika.raised.mixin.mods;
+
+import com.mojang.blaze3d.platform.Window;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import terrails.healthoverlay.render.HeartRenderer;
+
+@Mixin(HeartRenderer.class)
+public class HealthOverlayMixin {
+
+    @Redirect(method = "renderPlayerHearts", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;getGuiScaledHeight()I"))
+    private int modifyAddonsScaledHeight(Window instance) {
+        return instance.getGuiScaledHeight() - 2;
+    }
+
+}
