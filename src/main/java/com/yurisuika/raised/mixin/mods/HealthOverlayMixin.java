f70476c18d688a44e20e5c1621d1dda3f80cbfca
yurisuika <yurisuika@protonmail.com>
2022-04-17 20:38:50 -0700

    Health Overlay support

---
+++ src/main/java/com/yurisuika/raised/mixin/mods/HealthOverlayMixin.java
@@ -0,0 +1,17 @@
+package com.yurisuika.raised.mixin.mods;
+
+import net.minecraft.client.util.Window;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import terrails.healthoverlay.render.HeartRenderer;
+
+@Mixin(HeartRenderer.class)
+public class HealthOverlayMixin {
+
+    @Redirect(method = "renderPlayerHearts", at = @At(value = "INVOKE", target = "net/minecraft/client/util/Window.getScaledHeight()I"))
+    private int modifyAddonsScaledHeight(Window instance) {
+        return instance.getScaledHeight() - 2;
+    }
+
+}
