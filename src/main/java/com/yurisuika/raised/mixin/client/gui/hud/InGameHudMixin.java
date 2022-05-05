e6813a7f2da8b2bc7ed6769c773ffede42bba8b6
yurisuika <yurisuika@protonmail.com>
2022-05-05 05:42:44 -0700

    Adjustability + Fixed Inventorio Support

--- src/main/java/com/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/com/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
@@ -1,5 +1,6 @@
 package com.yurisuika.raised.mixin.client.gui.hud;
 
+import com.yurisuika.raised.Raised;
 import net.minecraft.client.gui.hud.InGameHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
@@ -9,7 +10,7 @@ public class InGameHudMixin {
 
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"), index = 2)
     private int modifyHotbar2(int value) {
-        return value - 2;
+        return value - Raised.getDistance();
     }
 
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V", ordinal = 1), index = 6)
@@ -19,37 +20,37 @@ public class InGameHudMixin {
 
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V"), index = 1)
     private int modifyItem2(int value) {
-        return value - 2;
+        return value - Raised.getDistance();
     }
 
     @ModifyVariable(method = "renderMountJumpBar", at = @At(value = "STORE"), ordinal = 3)
     private int modifyJumpBar2(int value) {
-        return value - 2;
+        return value - Raised.getDistance();
     }
 
     @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"), index = 2)
     private int modifyExperienceBar2(int value) {
-        return value - 2;
+        return value - Raised.getDistance();
     }
 
     @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/client/util/math/MatrixStack;Ljava/lang/String;FFI)I"), index = 3)
     private float modifyXpText2(float value) {
-        return value - (float)2;
+        return value - (float)Raised.getDistance();
     }
 
     @ModifyVariable(method = "renderHeldItemTooltip", at = @At(value = "STORE"), ordinal = 2)
     private int modifyHeldItemTooltip2(int value) {
-        return value - 2;
+        return value - Raised.getDistance();
     }
 
     @ModifyVariable(method = "renderStatusBars", at = @At(value = "STORE"), ordinal = 5)
     private int modifyStatusBars2(int value) {
-        return value - 2;
+        return value - Raised.getDistance();
     }
 
     @ModifyVariable(method = "renderMountHealth", at = @At(value = "STORE"), ordinal = 2)
     private int modifyMountHealth2(int value) {
-        return value - 2;
+        return value - Raised.getDistance();
     }
 
 }
--- src/main/java/com/yurisuika/raised/mixin/mods/InventorioMixin.java
+++ src/main/java/com/yurisuika/raised/mixin/mods/InventorioMixin.java
@@ -1,5 +1,6 @@
 package com.yurisuika.raised.mixin.mods;
 
+import com.yurisuika.raised.Raised;
 import me.lizardofoz.inventorio.client.ui.HotbarHUDRenderer;
 import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,12 +12,12 @@ public class InventorioMixin {
 
     @Redirect(method = "renderSegmentedHotbar", at = @At(value = "INVOKE", target = "net/minecraft/client/util/Window.getScaledHeight()I"))
     private int modifyScaledHeightA(Window instance) {
-        return instance.getScaledHeight() - 1;
+        return instance.getScaledHeight() - Raised.getDistance();
     }
 
     @Redirect(method = "renderHotbarAddons", at = @At(value = "INVOKE", target = "net/minecraft/client/util/Window.getScaledHeight()I"))
     private int modifyScaledHeightB(Window instance) {
-        return instance.getScaledHeight() - 2;
+        return instance.getScaledHeight() - Raised.getDistance();
     }
 
 }
