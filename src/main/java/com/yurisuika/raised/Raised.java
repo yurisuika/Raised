dcfe97923ac7595e65cc67a2d75e6dc11a2f8592
yurisuika <yurisuika@protonmail.com>
2022-05-19 05:51:50 -0700

    Added Config

--- src/main/java/com/yurisuika/raised/Raised.java
+++ src/main/java/com/yurisuika/raised/Raised.java
@@ -1,19 +1,25 @@
 package com.yurisuika.raised;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
 
 public class Raised implements ClientModInitializer {
 
-    public static final Logger LOGGER = LogManager.getLogger("Raised");
+    public static final Logger LOGGER = LoggerFactory.getLogger("raised");
 
     private static final KeyBinding down = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "raised.down",
@@ -29,22 +35,62 @@ public class Raised implements ClientModInitializer {
             "raised.title"
     ));
 
-    public static int distance = 2;
+    public static File file = new File(FabricLoader.getInstance().getConfigDir().toFile(), "raised.json");
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public static RaisedConfig config = new RaisedConfig();
+
+    public static void saveConfig() {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(gson.toJson(getConfig()));
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void loadConfig() {
+        try {
+            if (file.exists()) {
+                config = gson.fromJson(Files.readString(file.toPath()), RaisedConfig.class);
+            } else {
+                config = new RaisedConfig();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        setConfig(config);
+    }
+
+    public static void setConfig(RaisedConfig config) {
+        Raised.config = config;
+    }
+
+    public static RaisedConfig getConfig() {
+        return config;
+    }
 
     public static void setDistance(int change) {
-        distance += change;
-        FabricLoader.getInstance().getObjectShare().put("raised:distance", distance);
+        config.distance += change;
+        saveConfig();
+        putDistance();
     }
 
     public static int getDistance() {
-        return distance;
+        return config.distance;
+    }
+
+    public static void putDistance() {
+        FabricLoader.getInstance().getObjectShare().put("raised:distance", config.distance);
     }
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("Loading Raised!");
 
-        setDistance(0);
+        loadConfig();
+        putDistance();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (down.wasPressed()) {
---
+++ src/main/java/com/yurisuika/raised/RaisedConfig.java
@@ -0,0 +1,7 @@
+package com.yurisuika.raised;
+
+public class RaisedConfig {
+
+    public int distance = 2;
+
+}
