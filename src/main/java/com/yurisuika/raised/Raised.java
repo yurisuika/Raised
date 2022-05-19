46da589e3bb8efa6d8dafab6ed784a811eef510b
yurisuika <yurisuika@protonmail.com>
2022-05-19 05:51:43 -0700

    Added Config

--- src/main/java/com/yurisuika/raised/Raised.java
+++ src/main/java/com/yurisuika/raised/Raised.java
@@ -1,5 +1,7 @@
 package com.yurisuika.raised;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.client.ClientRegistry;
@@ -9,25 +11,67 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.loading.FMLPaths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
 
 @Mod("raised")
 public class Raised {
 
-    private static final Logger LOGGER = LogManager.getLogger();
-    public static int distance = 2;
-
-    public static void setDistance(int change) {
-        distance += change;
-    }
-
-    public static int getDistance() {
-        return distance;
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger("raised");
 
     public static KeyMapping down;
     public static KeyMapping up;
+
+    public static File file = new File(FMLPaths.CONFIGDIR.get().toFile(), "raised.json");
+    public static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public static RaisedConfig config = new RaisedConfig();
+
+    public static void saveConfig() {
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(gson.toJson(getConfig()));
+            fileWriter.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void loadConfig() {
+        try {
+            if (file.exists()) {
+                config = gson.fromJson(Files.readString(file.toPath()), RaisedConfig.class);
+            } else {
+                config = new RaisedConfig();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        setConfig(config);
+    }
+
+    public static void setConfig(RaisedConfig config) {
+        Raised.config = config;
+    }
+
+    public static RaisedConfig getConfig() {
+        return config;
+    }
+
+    public static void setDistance(int change) {
+        config.distance += change;
+        saveConfig();
+    }
+
+    public static int getDistance() {
+        return config.distance;
+    }
 
     public void input(InputEvent.KeyInputEvent event){
         if (down.consumeClick()) {
@@ -47,6 +91,8 @@ public class Raised {
     public void setup(final FMLClientSetupEvent event) {
         LOGGER.info("Loading Raised!");
         MinecraftForge.EVENT_BUS.addListener(this::input);
+
+        loadConfig();
 
         down = new KeyMapping("raised.down", KeyConflictContext.IN_GAME, InputConstants.getKey("key.keyboard.minus"), "raised.title");
         up = new KeyMapping("raised.up", KeyConflictContext.IN_GAME, InputConstants.getKey("key.keyboard.equal"), "raised.title");
