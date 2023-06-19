58154387d4e4ccdff027d0112988ffc178ad1b28
yurisuika <yurisuika@protonmail.com>
2023-06-19 01:11:15 -0700

    Replace Health Overlay support with Colorful Hearts / Remove unsupported mods

--- src/main/java/dev/yurisuika/raised/RaisedMixinPlugin.java
+++ src/main/java/dev/yurisuika/raised/RaisedMixinPlugin.java
@@ -13,31 +13,29 @@ import java.util.function.Supplier;
 
 public final class RaisedMixinPlugin implements IMixinConfigPlugin {
 
-    private static final Supplier<Boolean> TRUE = () -> true;
-
-    private static final Map<String, Supplier<Boolean>> CONDITIONS = ImmutableMap.of(
-            "dev.yurisuika.raised.mixin.mods.AppleskinMixin$HUDOverlayHandlerMixin", () -> FabricLoader.getInstance().isModLoaded("appleskin"),
-            "dev.yurisuika.raised.mixin.mods.BotaniaMixin$HUDHandlerMixin", () -> FabricLoader.getInstance().isModLoaded("botania"),
-            "dev.yurisuika.raised.mixin.mods.BotaniaMixin$FlugelTiaraItemMixin", () -> FabricLoader.getInstance().isModLoaded("botania"),
-            "dev.yurisuika.raised.mixin.mods.CreateMixin$CopperBacktankArmorLayerMixin", () -> FabricLoader.getInstance().isModLoaded("create"),
-            "dev.yurisuika.raised.mixin.mods.CreateMixin$SchematicHotbarSlotOverlayMixin", () -> FabricLoader.getInstance().isModLoaded("create"),
-            "dev.yurisuika.raised.mixin.mods.CreateMixin$ToolboxHandlerClientMixin", () -> FabricLoader.getInstance().isModLoaded("create"),
-            "dev.yurisuika.raised.mixin.mods.CreateMixin$ToolSelectionScreenMixin", () -> FabricLoader.getInstance().isModLoaded("create"),
-            "dev.yurisuika.raised.mixin.mods.CreateMixin$TrainHUDMixin", () -> FabricLoader.getInstance().isModLoaded("create"),
-            "dev.yurisuika.raised.mixin.mods.DetailArmorBarMixin$ArmorBarRendererMixin", () -> FabricLoader.getInstance().isModLoaded("detailab"),
-            "dev.yurisuika.raised.mixin.mods.FarmersDelightMixin$ComfortHealthOverlayMixin", () -> FabricLoader.getInstance().isModLoaded("farmersdelight")
+    private static final Map<String, Supplier<Boolean>> APPLESKIN = ImmutableMap.of(
+            "dev.yurisuika.raised.mixin.mods.AppleskinMixin$HUDOverlayHandlerMixin", () -> FabricLoader.getInstance().isModLoaded("appleskin")
     );
 
-    private static final Map<String, Supplier<Boolean>> CONDITIONS2 = ImmutableMap.of(
-            "dev.yurisuika.raised.mixin.mods.FarmersDelightMixin$NourishmentHungerOverlayMixin", () -> FabricLoader.getInstance().isModLoaded("farmersdelight"),
-            "dev.yurisuika.raised.mixin.mods.HealthOverlayMixin$HeartRendererMixin", () -> FabricLoader.getInstance().isModLoaded("healthoverlay"),
-            "dev.yurisuika.raised.mixin.mods.InventorioMixin$HotbarHUDRendererMixin", () -> FabricLoader.getInstance().isModLoaded("inventorio"),
+    private static final Map<String, Supplier<Boolean>> COLORFULHEARTS = ImmutableMap.of(
+            "dev.yurisuika.raised.mixin.mods.ColorfulHeartsMixin$HeartRendererMixin", () -> FabricLoader.getInstance().isModLoaded("colorfulhearts")
+    );
+
+    private static final Map<String, Supplier<Boolean>> DETAILARMORBAR = ImmutableMap.of(
+            "dev.yurisuika.raised.mixin.mods.DetailArmorBarMixin$ArmorBarRendererMixin", () -> FabricLoader.getInstance().isModLoaded("detailab")
+    );
+
+    private static final Map<String, Supplier<Boolean>> INVENTORIO = ImmutableMap.of(
+            "dev.yurisuika.raised.mixin.mods.InventorioMixin$HotbarHUDRendererMixin", () -> FabricLoader.getInstance().isModLoaded("inventorio")
+    );
+
+    private static final Map<String, Supplier<Boolean>> INVENTORYPROFILESNEXT = ImmutableMap.of(
             "dev.yurisuika.raised.mixin.mods.InventoryProfilesNextMixin$LockSlotsHandlerMixin", () -> FabricLoader.getInstance().isModLoaded("inventoryprofilesnext")
     );
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return CONDITIONS.getOrDefault(mixinClassName, TRUE).get() && CONDITIONS2.getOrDefault(mixinClassName, TRUE).get();
+        return APPLESKIN.getOrDefault(mixinClassName, () -> true).get() && COLORFULHEARTS.getOrDefault(mixinClassName, () -> true).get() && DETAILARMORBAR.getOrDefault(mixinClassName, () -> true).get() && INVENTORIO.getOrDefault(mixinClassName, () -> true).get() && INVENTORYPROFILESNEXT.getOrDefault(mixinClassName, () -> true).get();
     }
 
     @Override
