ef56de3815a8b2f1d6f9d1a53d98f35f38e51d34
yurisuika <yurisuika@protonmail.com>
2023-10-07 16:45:58 -0700

    consolidate method names

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
@@ -19,7 +19,7 @@ public abstract class InGameHudMixin {
 
         // HEAD
         @Inject(method = "render", at = @At("HEAD"))
-        private void headStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startHeadTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             if (getSupport()) {
                 context.getMatrices().translate(0, -getHud(), 0);
             }
@@ -27,107 +27,107 @@ public abstract class InGameHudMixin {
 
         // SPECTATOR MENU
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/SpectatorHud;renderSpectatorMenu(Lnet/minecraft/client/gui/DrawContext;)V"))
-        private void spectatorMenuStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startSpectatorMenuTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/SpectatorHud;renderSpectatorMenu(Lnet/minecraft/client/gui/DrawContext;)V", shift = At.Shift.AFTER))
-        private void spectatorMenuEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endSpectatorMenuTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // HOTBAR
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbar(FLnet/minecraft/client/gui/DrawContext;)V"))
-        private void hotbarStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startHotbarTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbar(FLnet/minecraft/client/gui/DrawContext;)V", shift = At.Shift.AFTER))
-        private void hotbarEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endHotbarTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // STATUS BARS
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderStatusBars(Lnet/minecraft/client/gui/DrawContext;)V"))
-        private void statusBarsStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startStatusBarsTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderStatusBars(Lnet/minecraft/client/gui/DrawContext;)V", shift = At.Shift.AFTER))
-        private void statusBarsEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endStatusBarsTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // MOUNT HEALTH BAR
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderMountHealth(Lnet/minecraft/client/gui/DrawContext;)V"))
-        private void mountHealthStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startMountHealthTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderMountHealth(Lnet/minecraft/client/gui/DrawContext;)V", shift = At.Shift.AFTER))
-        private void mountHealthEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endMountHealthTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // MOUNT JUMP BAR
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderMountJumpBar(Lnet/minecraft/entity/JumpingMount;Lnet/minecraft/client/gui/DrawContext;I)V"))
-        private void mountJumpBarStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startMountJumpBarTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderMountJumpBar(Lnet/minecraft/entity/JumpingMount;Lnet/minecraft/client/gui/DrawContext;I)V", shift = At.Shift.AFTER))
-        private void mountJumpBarEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endMountJumpBarTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // EXPERIENCE BAR
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderExperienceBar(Lnet/minecraft/client/gui/DrawContext;I)V"))
-        private void experienceBarStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startExperienceBarTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderExperienceBar(Lnet/minecraft/client/gui/DrawContext;I)V", shift = At.Shift.AFTER))
-        private void experienceBarEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endExperienceBarTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // HELD ITEM TOOLTIP
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHeldItemTooltip(Lnet/minecraft/client/gui/DrawContext;)V"))
-        private void heldItemTooltipStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startHeldItemTooltipTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHeldItemTooltip(Lnet/minecraft/client/gui/DrawContext;)V", shift = At.Shift.AFTER))
-        private void heldItemTooltipEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endHeldItemTooltipTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // SPECTATOR TOOLTIP
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/SpectatorHud;render(Lnet/minecraft/client/gui/DrawContext;)V"))
-        private void spectatorHudStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startSpectatorHudTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/SpectatorHud;render(Lnet/minecraft/client/gui/DrawContext;)V", shift = At.Shift.AFTER))
-        private void spectatorHudEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endSpectatorHudTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // OVERLAY MESSAGE
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V", ordinal = 0))
-        private void overlayMessageStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startOverlayMessageTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getHud(), 0);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V", ordinal = 0, shift = At.Shift.AFTER))
-        private void overlayMessageEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endOverlayMessageTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getHud(), 0);
         }
 
         // CHAT
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud;render(Lnet/minecraft/client/gui/DrawContext;III)V"))
-        private void chatStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startChatTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, -getChat(), +300);
         }
         @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud;render(Lnet/minecraft/client/gui/DrawContext;III)V", shift = At.Shift.AFTER))
-        private void chatEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endChatTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             context.getMatrices().translate(0, +getChat(), -300);
         }
 
         // TAIL
         @Inject(method = "render", at = @At("TAIL"))
-        private void tailStart(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void startTailTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             if (getSupport()) {
                 context.getMatrices().translate(0, -getHud(), 0);
             }
@@ -146,7 +146,7 @@ public abstract class InGameHudMixin {
 
         // HEAD
         @Inject(method = "render", at = @At("HEAD"))
-        private void headEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endHeadTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             if (getSupport()) {
                 context.getMatrices().translate(0, +getHud(), 0);
             }
@@ -154,7 +154,7 @@ public abstract class InGameHudMixin {
 
         // TAIL
         @Inject(method = "render", at = @At("TAIL"))
-        private void tailEnd(DrawContext context, float tickDelta, CallbackInfo ci) {
+        private void endTailTranslate(DrawContext context, float tickDelta, CallbackInfo ci) {
             if (getSupport()) {
                 context.getMatrices().translate(0, +getHud(), 0);
             }
