8b49d5fda4da8bae8e596cb7c1c4542c6c320bc8
yurisuika <yurisuika@protonmail.com>
2023-10-10 10:14:11 -0700

    go back to using separate texture for selector fix, but this time with mod namespace

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
@@ -2,13 +2,13 @@ package dev.yurisuika.raised.mixin.client.gui.hud;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import static dev.yurisuika.raised.client.option.RaisedConfig.*;
 
@@ -134,9 +134,10 @@ public abstract class InGameHudMixin {
         }
 
         // HOTBAR SELECTOR
-        @Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V"), locals = LocalCapture.CAPTURE_FAILHARD)
-        private void drawHotbarSelector(float tickDelta, DrawContext context, CallbackInfo ci, PlayerEntity playerEntity) {
-            context.drawTexture(new Identifier("textures/gui/sprites/hud/hotbar_selection.png"), (context.getScaledWindowWidth() / 2) - 92 + playerEntity.getInventory().selectedSlot * 20, context.getScaledWindowHeight(), 0, 0, 24, 1);
+        @ModifyArgs(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1))
+        private void resizeHotbarSelector(Args args) {
+            args.set(0, new Identifier("raised:hud/hotbar_selection"));
+            args.set(4, 24);
         }
 
     }
