c7ea65f2d72d20523b424303e52e5a978cdef450
yurisuika <yurisuika@protonmail.com>
2024-06-28 09:25:28 -0700

    break out for forge parity

--- src/main/java/dev/yurisuika/raised/util/Translate.java
+++ src/main/java/dev/yurisuika/raised/util/Translate.java
@@ -7,9 +7,17 @@ import net.minecraft.client.util.math.MatrixStack;
 
 public class Translate {
 
+    public static int getX(Element element) {
+        return Option.getX(Option.getSync(element) != Sync.NONE ? Element.byId(Option.getSync(element).getId()) : element) * Option.getPosition(element).getX();
+    }
+
+    public static int getY(Element element) {
+        return Option.getY(Option.getSync(element) != Sync.NONE ? Element.byId(Option.getSync(element).getId()) : element) * Option.getPosition(element).getY();
+    }
+
     public static void start(MatrixStack matrixStack, Element element) {
         matrixStack.push();
-        matrixStack.translate(Option.getX(Option.getSync(element) != Sync.NONE ? Element.byId(Option.getSync(element).getId()) : element) * Option.getPosition(element).getX(), Option.getY(Option.getSync(element) != Sync.NONE ? Element.byId(Option.getSync(element).getId()) : element) * Option.getPosition(element).getY(), 0);
+        matrixStack.translate(getX(element), getY(element), 0);
     }
 
     public static void end(MatrixStack matrixStack) {
