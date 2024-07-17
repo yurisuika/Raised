8d57dc299997ce72503d4bfe23ed41f2e1634a7f
yurisuika <yurisuika@protonmail.com>
2024-07-17 02:19:04 -0700

    reorganize stuff

--- src/main/java/dev/yurisuika/raised/client/commands/RaisedCommand.java
+++ src/main/java/dev/yurisuika/raised/client/commands/RaisedCommand.java
@@ -4,13 +4,10 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import dev.yurisuika.raised.commands.arguments.PositionArgument;
 import dev.yurisuika.raised.commands.arguments.SyncArgument;
+import dev.yurisuika.raised.config.Options;
 import dev.yurisuika.raised.util.config.Config;
 import dev.yurisuika.raised.util.config.Option;
-import dev.yurisuika.raised.util.config.option.Elements;
-import dev.yurisuika.raised.util.config.option.Properties;
-import dev.yurisuika.raised.util.type.Element;
-import dev.yurisuika.raised.util.type.Position;
-import dev.yurisuika.raised.util.type.Sync;
+import dev.yurisuika.raised.util.properties.Element;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.TranslatableComponent;
@@ -29,56 +26,7 @@ public class RaisedCommand {
                         )
                         .then(Commands.literal("reset")
                                 .executes(commandContext -> {
-                                    Option.setElements(new Elements(
-                                            new Properties.Hotbar(
-                                                    0,
-                                                    2,
-                                                    Position.BOTTOM,
-                                                    Sync.NONE
-                                            ),
-                                            new Properties.Chat(
-                                                    0,
-                                                    0,
-                                                    Position.BOTTOM,
-                                                    Sync.NONE
-                                            ),
-                                            new Properties.Bossbar(
-                                                    0,
-                                                    0,
-                                                    Position.TOP,
-                                                    Sync.NONE
-                                            ),
-                                            new Properties.Sidebar(
-                                                    0,
-                                                    0,
-                                                    Position.RIGHT,
-                                                    Sync.NONE
-                                            ),
-                                            new Properties.Effects(
-                                                    0,
-                                                    0,
-                                                    Position.TOP_RIGHT,
-                                                    Sync.NONE
-                                            ),
-                                            new Properties.Players(
-                                                    0,
-                                                    0,
-                                                    Position.TOP,
-                                                    Sync.NONE
-                                            ),
-                                            new Properties.Toasts(
-                                                    0,
-                                                    0,
-                                                    Position.TOP_RIGHT,
-                                                    Sync.NONE
-                                            ),
-                                            new Properties.Other(
-                                                    0,
-                                                    0,
-                                                    Position.BOTTOM,
-                                                    Sync.NONE
-                                            )
-                                    ));
+                                    Option.setLayers(new Options().getLayers());
                                     commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.config.reset"), false);
                                     return 1;
                                 })
@@ -88,56 +36,56 @@ public class RaisedCommand {
 
         for (Element element : Element.values()) {
             dispatcher.register(Commands.literal("raised")
-                    .then(Commands.literal("elements")
+                    .then(Commands.literal("layers")
                             .then(Commands.literal(element.getSerializedName())
                                     .then(Commands.literal("x")
                                             .executes(commandContext -> {
-                                                commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.elements.element.x.query", new TranslatableComponent(element.getKey()), Option.getY(element)), false);
+                                                commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.layers.element.x.query", new TranslatableComponent(element.getKey()), Option.getY(element)), false);
                                                 return 1;
                                             })
                                             .then(Commands.argument("x", IntegerArgumentType.integer(0))
                                                     .executes(commandContext -> {
                                                         Option.setX(element, IntegerArgumentType.getInteger(commandContext, "x"));
-                                                        commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.elements.element.x.set", new TranslatableComponent(element.getKey()), Option.getX(element)), false);
+                                                        commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.layers.element.x.set", new TranslatableComponent(element.getKey()), Option.getX(element)), false);
                                                         return 1;
                                                     })
                                             )
                                     )
                                     .then(Commands.literal("y")
                                             .executes(commandContext -> {
-                                                commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.elements.element.y.query", new TranslatableComponent(element.getKey()), Option.getY(element)), false);
+                                                commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.layers.element.y.query", new TranslatableComponent(element.getKey()), Option.getY(element)), false);
                                                 return 1;
                                             })
                                             .then(Commands.argument("y", IntegerArgumentType.integer(0))
                                                     .executes(commandContext -> {
                                                         Option.setY(element, IntegerArgumentType.getInteger(commandContext, "y"));
-                                                        commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.elements.element.y.set", new TranslatableComponent(element.getKey()), Option.getY(element)), false);
+                                                        commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.layers.element.y.set", new TranslatableComponent(element.getKey()), Option.getY(element)), false);
                                                         return 1;
                                                     })
                                             )
                                     )
                                     .then(Commands.literal("position")
                                             .executes(commandContext -> {
-                                                commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.elements.element.position.query", new TranslatableComponent(element.getKey()), new TranslatableComponent(Option.getPosition(element).getSerializedName())), false);
+                                                commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.layers.element.position.query", new TranslatableComponent(element.getKey()), new TranslatableComponent(Option.getPosition(element).getSerializedName())), false);
                                                 return 1;
                                             })
                                             .then(Commands.argument("position", PositionArgument.position())
                                                     .executes(commandContext -> {
                                                         Option.setPosition(element, PositionArgument.getPosition(commandContext, "position"));
-                                                        commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.elements.element.position.set", new TranslatableComponent(element.getKey()), new TranslatableComponent(Option.getPosition(element).getSerializedName())), false);
+                                                        commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.layers.element.position.set", new TranslatableComponent(element.getKey()), new TranslatableComponent(Option.getPosition(element).getSerializedName())), false);
                                                         return 1;
                                                     })
                                             )
                                     )
                                     .then(Commands.literal("sync")
                                             .executes(commandContext -> {
-                                                commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.elements.element.sync.query", new TranslatableComponent(element.getKey()), new TranslatableComponent(Option.getSync(element).getSerializedName())), false);
+                                                commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.layers.element.sync.query", new TranslatableComponent(element.getKey()), new TranslatableComponent(Option.getSync(element).getSerializedName())), false);
                                                 return 1;
                                             })
                                             .then(Commands.argument("sync", SyncArgument.sync())
                                                     .executes(commandContext -> {
                                                         Option.setSync(element, SyncArgument.getSync(commandContext, "sync"));
-                                                        commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.elements.element.sync.set", new TranslatableComponent(element.getKey()), new TranslatableComponent(Option.getSync(element).getSerializedName())), false);
+                                                        commandContext.getSource().sendSuccess(new TranslatableComponent("commands.raised.layers.element.sync.set", new TranslatableComponent(element.getKey()), new TranslatableComponent(Option.getSync(element).getSerializedName())), false);
                                                         return 1;
                                                     })
                                             )
