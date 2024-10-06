b600065c1fb024ed564940b45510aaf6b143a8f2
yurisuika <yurisuika@protonmail.com>
2024-10-06 12:04:55 -0700

    abstract mixin

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/components/toasts/ToastComponentMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/components/toasts/ToastComponentMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-public class ToastComponentMixin {
+public abstract class ToastComponentMixin {
 
     public abstract static class Toasts {
 
