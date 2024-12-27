968bed6fa3c6789382df07cd35fde836f7f4f726
yurisuika <yurisuika@protonmail.com>
2024-12-27 02:36:51 -0800

    reorganize registration

--- src/main/java/dev/yurisuika/raised/client/gui/components/IconToggleButton.java
+++ src/main/java/dev/yurisuika/raised/client/gui/components/IconToggleButton.java
@@ -34,7 +34,6 @@ public class IconToggleButton extends Button {
         return toggled;
     }
 
-    @Override
     public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         if (texture != null) {
             RenderSystem.disableDepthTest();
@@ -44,7 +43,6 @@ public class IconToggleButton extends Button {
         }
     }
 
-    @Override
     public void renderString(GuiGraphics guiGraphics, Font font, int color) {}
 
     public static class Builder {
