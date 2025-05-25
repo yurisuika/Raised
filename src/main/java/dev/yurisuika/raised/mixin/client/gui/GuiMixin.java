d699debf45e327de7e23cd6e45fae4c3e145fb66
yurisuika <yurisuika@protonmail.com>
2025-05-25 01:46:01 -0700

    Initial 5.x update push

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/GuiMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/GuiMixin.java
@@ -2,7 +2,7 @@ package dev.yurisuika.raised.mixin.client.gui;
 
 import dev.yurisuika.raised.util.Pack;
 import dev.yurisuika.raised.util.config.Option;
-import dev.yurisuika.raised.util.resources.Texture;
+import dev.yurisuika.raised.util.config.options.Resource;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
@@ -26,7 +26,7 @@ public abstract class GuiMixin {
              */
             @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V", ordinal = 1), index = 0)
             private ResourceLocation replaceHotbarSelectorIdentifier(ResourceLocation texture) {
-                if (Option.getTexture() == Texture.REPLACE || (Option.getTexture() == Texture.AUTO && Pack.getPack())) {
+                if (Option.getTexture() == Resource.Texture.REPLACE || (Option.getTexture() == Resource.Texture.AUTO && Pack.getPack())) {
                     return ResourceLocation.tryParse("raised:hud/hotbar_selection");
                 } else {
                     return texture;
@@ -35,7 +35,7 @@ public abstract class GuiMixin {
 
             @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V", ordinal = 1), index = 4)
             private int replaceHotbarSelectorHeight(int height) {
-                if (Option.getTexture() == Texture.REPLACE || (Option.getTexture() == Texture.AUTO && Pack.getPack())) {
+                if (Option.getTexture() == Resource.Texture.REPLACE || (Option.getTexture() == Resource.Texture.AUTO && Pack.getPack())) {
                     return 24;
                 } else {
                     return height;
@@ -47,7 +47,7 @@ public abstract class GuiMixin {
              */
             @Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V"), locals = LocalCapture.CAPTURE_FAILHARD)
             private void patchHotbarSelector(float partialTick, GuiGraphics guiGraphics, CallbackInfo ci, Player player) {
-                if (Option.getTexture() == Texture.PATCH  || (Option.getTexture() == Texture.AUTO && !Pack.getPack())) {
+                if (Option.getTexture() == Resource.Texture.PATCH  || (Option.getTexture() == Resource.Texture.AUTO && !Pack.getPack())) {
                     int x = (guiGraphics.guiWidth() / 2) - 92 + player.getInventory().selected * 20;
                     int y = guiGraphics.guiHeight();
                     ((GuiGraphicsInvoker) guiGraphics).invokeInnerBlit(ResourceLocation.tryParse("textures/gui/sprites/hud/hotbar_selection.png"), x, x + 24, y, y + 1, 0, 0, 1, 1 / 23.0F, 0);
---
+++ src/main/java/dev/yurisuika/raised/util/Validate.java
@@ -0,0 +1,23 @@
+package dev.yurisuika.raised.util;
+
+import dev.yurisuika.raised.config.Options;
+import dev.yurisuika.raised.util.config.Option;
+import dev.yurisuika.raised.util.config.options.Layer;
+
+public class Validate {
+
+    public static void checkForOldConfig() {
+        Option.getLayers().forEach((name, layer) -> {
+            if (layer.getDisplacement() == null) {
+                resetConfig();
+            }
+        });
+    }
+
+    public static void resetConfig() {
+        Option.setLayers(new Options().getLayers());
+        Option.setResource(new Options().getResource());
+        Layers.OTHER_LAYERS.forEach(id -> Option.addLayer(id.toString(), new Layer(new Layer.Displacement(0, 0), new Layer.Direction(Layer.Direction.X.NONE, Layer.Direction.Y.NONE), id.toString())));
+    }
+
+}
