594bb5e9d96a171cafcc8b3191ee44c0d2f88d3d
yurisuika <yurisuika@protonmail.com>
2025-05-25 01:46:10 -0700

    Initial 5.x update push

---
+++ src/main/java/dev/yurisuika/raised/util/Validate.java
@@ -0,0 +1,22 @@
+package dev.yurisuika.raised.util;
+
+import dev.yurisuika.raised.config.Options;
+import dev.yurisuika.raised.util.config.Option;
+import dev.yurisuika.raised.util.config.options.Layer;
+
+public class Validate {
+
+    public static void checkForOldConfig() {
+        Option.getLayers().forEach((name, layer) -> {
+            if (layer.getDisplacement() == null) {
+                resetConfig();
+            }
+        });
+    }
+
+    public static void resetConfig() {
+        Option.setLayers(new Options().getLayers());
+        Option.setResource(new Options().getResource());
+    }
+
+}
