ce937c6237dc3f4c959dc574f214a0e5ddcacd64
yurisuika <yurisuika@protonmail.com>
2025-05-25 01:47:35 -0700

    Initial 5.x update push

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/screens/ChatScreenMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/screens/ChatScreenMixin.java
@@ -1,7 +1,8 @@
 package dev.yurisuika.raised.mixin.client.gui.screens;
 
+import dev.yurisuika.raised.util.Layers;
 import dev.yurisuika.raised.util.Translate;
-import dev.yurisuika.raised.util.properties.Element;
+import dev.yurisuika.raised.util.config.options.Layer;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.ChatScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,11 +18,11 @@ public abstract class ChatScreenMixin {
         public abstract static class Pre {
 
             /**
-             * Moves the {@code chat} for {@link Element.CHAT}.
+             * Moves the {@code chat} for {@link Layer} key "minecraft:chat".
              */
             @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/ChatComponent;render(Lnet/minecraft/client/gui/GuiGraphics;IIIZ)V"))
             private void startChatTranslate(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
-                Translate.start(guiGraphics.pose(), Element.CHAT);
+                Translate.start(guiGraphics.pose(), Layers.CHAT.toString());
             }
 
             @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/ChatComponent;render(Lnet/minecraft/client/gui/GuiGraphics;IIIZ)V", shift = At.Shift.AFTER))
