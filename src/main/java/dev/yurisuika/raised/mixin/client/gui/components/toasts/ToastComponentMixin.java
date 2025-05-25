41f742f932f406cbf382ff7adfad3f451d7fc919
yurisuika <yurisuika@protonmail.com>
2025-05-25 01:47:11 -0700

    Initial 5.x update push

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/components/toasts/ToastComponentMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/components/toasts/ToastComponentMixin.java
@@ -1,7 +1,8 @@
 package dev.yurisuika.raised.mixin.client.gui.components.toasts;
 
+import dev.yurisuika.raised.util.Layers;
 import dev.yurisuika.raised.util.Translate;
-import dev.yurisuika.raised.util.properties.Element;
+import dev.yurisuika.raised.util.config.options.Layer;
 import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,11 +17,11 @@ public abstract class ToastComponentMixin {
         public abstract static class Pre {
 
             /**
-             * Moves the {@code toasts} if {@link Element.TOASTS} is enabled.
+             * Moves the {@code toasts} for {@link Layer} key "minecraft:toasts".
              */
             @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V"))
             private void startToastsTranslate(int x, GuiGraphics guiGraphics, CallbackInfoReturnable<Boolean> cir) {
-                Translate.start(guiGraphics.pose(), Element.TOASTS);
+                Translate.start(guiGraphics.pose(), Layers.TOASTS.toString());
             }
 
             @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V", shift = At.Shift.AFTER))
