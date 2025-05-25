cf77e3a660aa2235b05d85d6415c9aad434446e3
yurisuika <yurisuika@protonmail.com>
2025-05-25 01:46:59 -0700

    Initial 5.x update push

--- src/main/java/dev/yurisuika/raised/client/gui/components/LayerButton.java
+++ src/main/java/dev/yurisuika/raised/client/gui/components/LayerButton.java
@@ -2,23 +2,24 @@ package dev.yurisuika.raised.client.gui.components;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
-public class IconToggleButton extends Button {
+public class LayerButton extends Button {
 
+    public final Component message;
     public final ResourceLocation texture;
-    public final int textureWidth;
-    public final int textureHeight;
+    public final int textureSize;
     public boolean toggled;
     public static final ResourceLocation TEXTURE = ResourceLocation.tryParse("textures/gui/widgets.png");
 
-    public IconToggleButton(int x, int y, int width, int height, Component message, int textureWidth, int textureHeight, ResourceLocation texture, OnPress onPress, CreateNarration createNarration, boolean toggled) {
+    public LayerButton(int x, int y, int width, int height, Component message, int textureSize, ResourceLocation texture, OnPress onPress, CreateNarration createNarration, boolean toggled) {
         super(x, y, width, height, message, onPress, createNarration == null ? DEFAULT_NARRATION : createNarration);
-        this.textureWidth = textureWidth;
-        this.textureHeight = textureHeight;
+        this.message = message;
+        this.textureSize = textureSize;
         this.texture = texture;
         this.toggled = toggled;
     }
@@ -31,6 +32,7 @@ public class IconToggleButton extends Button {
         return toggled;
     }
 
+    @Override
     public void renderButton(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
         if (texture != null) {
             RenderSystem.disableDepthTest();
@@ -48,7 +50,8 @@ public class IconToggleButton extends Button {
             blit(poseStack, getX(), getY(), 0, v, width / 2, height);
             blit(poseStack, getX() + (width / 2), getY(), 200 - (width / 2), v, width / 2, height);
             RenderSystem.setShaderTexture(0, texture);
-            blit(poseStack, getX(), getY(), 0, 0, getWidth(), getHeight(), getWidth(), getHeight());
+            blit(poseStack, getX(), getY(), 0, 0, textureSize, textureSize, textureSize, textureSize);
+            ((AbstractWidgetInterface) this).renderScrollingString(poseStack, Minecraft.getInstance().font, message, getX() + textureSize, getY(), getX() + getWidth() - 2, getY() + getHeight(), -1);
             RenderSystem.enableDepthTest();
         }
     }
@@ -63,8 +66,7 @@ public class IconToggleButton extends Button {
         public int width = 150;
         public int height = 20;
         public ResourceLocation texture;
-        public int textureWidth;
-        public int textureHeight;
+        public int textureSize;
         public CreateNarration createNarration;
         public boolean toggled;
 
@@ -80,30 +82,19 @@ public class IconToggleButton extends Button {
             return this;
         }
 
-        public Builder width(int width) {
-            this.width = width;
-            return this;
-        }
-
         public Builder size(int width, int height) {
             this.width = width;
             this.height = height;
             return this;
         }
-
-        public Builder bounds(int x, int y, int width, int height) {
-            return pos(x, y).size(width, height);
-        }
-
         public Builder tooltip(Tooltip tooltip) {
             this.tooltip = tooltip;
             return this;
         }
 
-        public Builder texture(ResourceLocation texture, int width, int height) {
+        public Builder texture(ResourceLocation texture, int textureSize) {
             this.texture = texture;
-            this.textureWidth = width;
-            this.textureHeight = height;
+            this.textureSize = textureSize;
             return this;
         }
 
@@ -112,14 +103,10 @@ public class IconToggleButton extends Button {
             return this;
         }
 
-        public IconToggleButton build() {
-            if (texture == null) {
-                throw new IllegalStateException("Sprite not set");
-            } else {
-                IconToggleButton iconToggleButtonWidget = new IconToggleButton(x, y, width, height, message, textureWidth, textureHeight, texture, onPress, createNarration, toggled);
-                iconToggleButtonWidget.setTooltip(tooltip);
-                return iconToggleButtonWidget;
-            }
+        public LayerButton build() {
+            LayerButton layerButtonWidget = new LayerButton(x, y, width, height, message, textureSize, texture, onPress, createNarration, toggled);
+            layerButtonWidget.setTooltip(tooltip);
+            return layerButtonWidget;
         }
 
     }
