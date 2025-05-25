6cc6cbcef74e95ff57c4f3e85ad4d05c37b303bb
yurisuika <yurisuika@protonmail.com>
2025-05-25 01:46:37 -0700

    Initial 5.x update push

---
+++ src/main/java/dev/yurisuika/raised/client/gui/components/LayerList.java
@@ -0,0 +1,132 @@
+package dev.yurisuika.raised.client.gui.components;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import dev.yurisuika.raised.client.gui.screens.RaisedScreen;
+import dev.yurisuika.raised.util.Parse;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.ContainerObjectSelectionList;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.narration.NarratableEntry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.List;
+
+public class LayerList extends ContainerObjectSelectionList<LayerList.Entry> {
+
+    public RaisedScreen screen;
+
+    public LayerList(Minecraft minecraft, int width, int height, RaisedScreen screen) {
+        super(minecraft, width, height, 0, height, screen.BUTTON_HEIGHT + screen.SPACING);
+        this.screen = screen;
+        ((AbstractSelectionListInterface) this).setPadding(screen.PADDING);
+        setRenderBackground(false);
+        setRenderTopAndBottom(false);
+        setRenderHeader(false, 0);
+    }
+
+    @Override
+    public int getRowWidth() {
+        return screen.BUTTON_WIDTH;
+    }
+
+    public void add(String name) {
+        addEntry(new Entry(screen, name));
+    }
+
+    @Override
+    public int getMaxScroll() {
+        return Math.max(0, getMaxPosition() - height);
+    }
+
+    @Override
+    public int getMaxPosition() {
+        return screen.PADDING + headerHeight + (getItemCount() * itemHeight) - screen.SPACING + screen.PADDING;
+    }
+
+    @Override
+    public void renderDecorations(PoseStack poseStack, int mouseX, int mouseY) {
+        for (int i = 0; i < children().size(); i++) {
+            if (getEntry(i).button.isHoveredOrFocused()) {
+                getEntry(i).button.renderToolTip(poseStack, mouseX, mouseY);
+            }
+        }
+    }
+
+    @Override
+    public int getRowTop(int index) {
+        return y0 + screen.PADDING - (int) getScrollAmount() + index * itemHeight + headerHeight;
+    }
+
+    @Override
+    public int getRowLeft() {
+        return x0 + screen.PADDING;
+    }
+
+    @Override
+    public int getScrollbarPosition() {
+        return getRowRight() + screen.PADDING - 6;
+    }
+
+    public static class Entry extends ContainerObjectSelectionList.Entry<Entry> {
+
+        public final RaisedScreen screen;
+        public final String name;
+        public final LayerButton button;
+
+        public Entry(RaisedScreen screen, String name) {
+            this.screen = screen;
+            this.name = name;
+            this.button = createLayerButton(name);
+        }
+
+        public void updateButtonStates() {
+            button.toggled = RaisedScreen.current.toString().equals(name);
+            button.active = !button.toggled;
+        }
+
+        public LayerButton createLayerButton(String name) {
+            String texture;
+            ResourceLocation id = ResourceLocation.tryParse(name);
+            if (id.getNamespace().equals(ResourceLocation.DEFAULT_NAMESPACE)) {
+                texture = switch (id.getPath()) {
+                    case "hotbar" -> "hotbar";
+                    case "chat" -> "chat";
+                    case "bossbar" -> "bossbar";
+                    case "sidebar" -> "sidebar";
+                    case "effects" -> "effects";
+                    case "players" -> "players";
+                    case "toasts" -> "toasts";
+                    default -> "other";
+                };
+            } else {
+                texture = "other";
+            }
+
+            return LayerButton.builder(Parse.createLayerDisplay(name), button -> {
+                RaisedScreen.current = id;
+                screen.resetOptions();
+                screen.layerList.children().forEach(Entry::updateButtonStates);
+            }, name.equals(RaisedScreen.current.toString())).size(screen.BUTTON_WIDTH, screen.BUTTON_HEIGHT).texture(ResourceLocation.tryParse("raised:textures/gui/icon/" + texture + ".png"), screen.BUTTON_HEIGHT).tooltip((button, poseStack, mouseX, mouseY) -> screen.renderTooltip(poseStack, Component.literal(name), mouseX, mouseY)).build();
+        }
+
+        @Override
+        public void render(PoseStack poseStack, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean hovering, float partialTick) {
+            button.y = top;
+            button.x = left;
+            button.render(poseStack, mouseX, mouseY, partialTick);
+        }
+
+        @Override
+        public List<? extends GuiEventListener> children() {
+            return List.of(button);
+        }
+
+        @Override
+        public List<? extends NarratableEntry> narratables() {
+            return List.of(button);
+        }
+
+    }
+
+}
---
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/components/AbstractWidgetMixin.java
@@ -0,0 +1,52 @@
+package dev.yurisuika.raised.mixin.client.gui.components;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import dev.yurisuika.raised.client.gui.components.AbstractWidgetInterface;
+import net.minecraft.Util;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(AbstractWidget.class)
+public abstract class AbstractWidgetMixin extends GuiComponent implements AbstractWidgetInterface {
+
+    @Shadow
+    public int x;
+    @Shadow
+    public int y;
+    @Shadow
+    protected int width;
+    @Shadow
+    protected int height;
+
+    @Redirect(method = "renderButton", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/AbstractWidget;drawCenteredString(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;III)V"))
+    private void drawScrollingString(PoseStack poseStack, Font font, Component component, int stringX, int stringY, int color) {
+        renderScrollingString(poseStack, font, component, x + 2, y, x + width - 2, y + height, color);
+    }
+
+    @Override
+    public void renderScrollingString(PoseStack poseStack, Font font, Component text, int minX, int minY, int maxX, int maxY, int color) {
+        int i = font.width(text);
+        int j = (minY + maxY - 9) / 2 + 1;
+        int k = maxX - minX;
+        if (i > k) {
+            int l = i - k;
+            double d0 = Util.getMillis() / 1000.0D;
+            double d1 = Math.max(l * 0.5D, 3.0D);
+            double d2 = Math.sin((Math.PI / 2) * Math.cos((Math.PI * 2) * d0 / d1)) / 2.0D + 0.5D;
+            double d3 = Mth.lerp(d2, 0.0D, l);
+            enableScissor(minX, minY, maxX, maxY);
+            drawString(poseStack, font, text, minX - (int) d3, j, color);
+            disableScissor();
+        } else {
+            drawCenteredString(poseStack, font, text, (minX + maxX) / 2, j, color);
+        }
+    }
+
+}
