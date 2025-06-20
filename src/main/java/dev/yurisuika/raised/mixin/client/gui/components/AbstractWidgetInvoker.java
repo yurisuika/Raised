1cee73fa8f23f5fcd2e6e00b682bbd5fab751e87
yurisuika <yurisuika@protonmail.com>
2025-06-19 18:12:35 -0700

    config screen list rework

---
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/components/AbstractWidgetInvoker.java
@@ -0,0 +1,18 @@
+package dev.yurisuika.raised.mixin.client.gui.components;
+
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(AbstractWidget.class)
+public interface AbstractWidgetInvoker {
+
+    @Invoker("renderScrollingString")
+    static void invokeRenderScrollingString(GuiGraphics guiGraphics, Font font, Component text, int minX, int minY, int maxX, int maxY, int color) {
+        throw new AssertionError();
+    }
+
+}
