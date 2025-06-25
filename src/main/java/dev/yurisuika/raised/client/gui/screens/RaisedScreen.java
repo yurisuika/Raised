3ad29cf38403d0bd2a3f5f6baef464193217d7ff
yurisuika <yurisuika@protonmail.com>
2025-06-25 04:09:49 -0700

    Tidying up.

--- src/main/java/dev/yurisuika/raised/client/gui/screens/RaisedScreen.java
+++ src/main/java/dev/yurisuika/raised/client/gui/screens/RaisedScreen.java
@@ -2,12 +2,12 @@ package dev.yurisuika.raised.client.gui.screens;
 
 import com.mojang.serialization.Codec;
 import dev.yurisuika.raised.client.RaisedOptions;
-import dev.yurisuika.raised.client.gui.Layers;
+import dev.yurisuika.raised.client.gui.Layer;
 import dev.yurisuika.raised.client.gui.components.LayerList;
 import dev.yurisuika.raised.mixin.client.gui.components.AbstractWidgetInvoker;
+import dev.yurisuika.raised.registry.LayerRegistry;
+import dev.yurisuika.raised.util.Configure;
 import dev.yurisuika.raised.util.Parse;
-import dev.yurisuika.raised.util.config.Option;
-import dev.yurisuika.raised.util.config.options.Layer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.OptionInstance;
 import net.minecraft.client.Options;
@@ -24,16 +24,11 @@ import java.util.*;
 
 public class RaisedScreen extends Screen {
 
-    public Screen lastScreen;
+    public Screen parent;
     public ArrayList<AbstractWidget> options;
     public ArrayList<AbstractWidget> controls;
-    public LayerList list;
-    public AbstractWidget displacementX;
-    public AbstractWidget displacementY;
-    public AbstractWidget directionX;
-    public AbstractWidget directionY;
-    public AbstractWidget sync;
-    public static ResourceLocation current = Layers.HOTBAR;
+    public LayerList layers;
+    public static ResourceLocation current = LayerRegistry.HOTBAR;
     public final int SPACING = 5;
     public final int PADDING = 8;
     public final int WIDGET_WIDTH_WIDE = 150;
@@ -42,16 +37,16 @@ public class RaisedScreen extends Screen {
     public final int HEADER_HEIGHT = PADDING + WIDGET_HEIGHT + PADDING;
     public final int PANEL_WIDTH = PADDING + WIDGET_WIDTH_WIDE + PADDING;
 
-    public RaisedScreen(Screen lastScreen) {
+    public RaisedScreen(Screen parent) {
         super(Component.translatable("options.raised.title"));
-        this.lastScreen = lastScreen;
+        this.parent = parent;
     }
 
     @Override
     public void init() {
         addOptions();
         addControls();
-        addList();
+        addLayers();
 
         repositionElements();
     }
@@ -59,17 +54,11 @@ public class RaisedScreen extends Screen {
     public void addOptions() {
         options = new ArrayList<>();
 
-        displacementX = new OptionInstance<>("options.raised.displacement.x", OptionInstance.cachedConstantTooltip(Component.translatable("options.raised.displacement.x.tooltip")), (prefix, value) -> value == 0 ? Options.genericValueLabel(prefix, CommonComponents.OPTION_OFF) : Options.genericValueLabel(prefix, Component.literal(Option.getDisplacementX(current.toString()) + "px (" + Math.round(Math.ceil((value.floatValue() / ((float) width / 4)) * 100)) + "%)")), new OptionInstance.IntRange(0, width / 4), Option.getDisplacementX(current.toString()), value -> Option.setDisplacementX(current.toString(), value)).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING, WIDGET_WIDTH_WIDE);
-        displacementY = new OptionInstance<>("options.raised.displacement.y", OptionInstance.cachedConstantTooltip(Component.translatable("options.raised.displacement.y.tooltip")), (prefix, value) -> value == 0 ? Options.genericValueLabel(prefix, CommonComponents.OPTION_OFF) : Options.genericValueLabel(prefix, Component.literal(Option.getDisplacementY(current.toString()) + "px (" + Math.round(Math.ceil((value.floatValue() / ((float) height / 4)) * 100)) + "%)")), new OptionInstance.IntRange(0, height / 4), Option.getDisplacementY(current.toString()), value -> Option.setDisplacementY(current.toString(), value)).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING + (WIDGET_HEIGHT + SPACING), WIDGET_WIDTH_WIDE);
-        directionX = new OptionInstance<>("options.raised.direction.x", value -> Tooltip.create(Component.translatable("options.raised.direction.x.tooltip")), OptionInstance.forOptionEnum(), new OptionInstance.Enum<>(Arrays.asList(Layer.Direction.X.values()), Codec.INT.xmap(Layer.Direction.X::byId, Layer.Direction.X::getId)), Layer.Direction.X.byName(Option.getDirectionX(current.toString()).getSerializedName()), value -> Option.setDirectionX(current.toString(), value)).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING + (WIDGET_HEIGHT + SPACING) * 2, WIDGET_WIDTH_WIDE);
-        directionY = new OptionInstance<>("options.raised.direction.y", value -> Tooltip.create(Component.translatable("options.raised.direction.y.tooltip")), OptionInstance.forOptionEnum(), new OptionInstance.Enum<>(Arrays.asList(Layer.Direction.Y.values()), Codec.INT.xmap(Layer.Direction.Y::byId, Layer.Direction.Y::getId)), Layer.Direction.Y.byName(Option.getDirectionY(current.toString()).getSerializedName()), value -> Option.setDirectionY(current.toString(), value)).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING + (WIDGET_HEIGHT + SPACING) * 3, WIDGET_WIDTH_WIDE);
-        sync = new OptionInstance<>("options.raised.sync", value -> Tooltip.create(Component.translatable("options.raised.sync.tooltip", value.toString())), (prefix, value) -> Parse.createLayerDisplay(value), new OptionInstance.Enum<>(Layers.LAYERS.keySet().stream().sorted(Comparator.comparing(ResourceLocation::toString)).toList(), ResourceLocation.CODEC), ResourceLocation.tryParse(Option.getSync(current.toString())), value -> Option.setSync(current.toString(), value.toString())).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING + (WIDGET_HEIGHT + SPACING) * 4, WIDGET_WIDTH_WIDE);
-
-        options.add(displacementX);
-        options.add(displacementY);
-        options.add(directionX);
-        options.add(directionY);
-        options.add(sync);
+        options.add(new OptionInstance<>("options.raised.displacement.x", OptionInstance.cachedConstantTooltip(Component.translatable("options.raised.displacement.x.tooltip")), (prefix, value) -> Options.genericValueLabel(prefix, value == 0 ? CommonComponents.OPTION_OFF : Component.literal(Configure.getDisplacementX(current.toString()) + "px (" + Math.round(Math.ceil((value.floatValue() / ((float) width / 4)) * 100)) + "%)")), new OptionInstance.IntRange(0, width / 4), Configure.getDisplacementX(current.toString()), value -> Configure.setDisplacementX(current.toString(), value)).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING, WIDGET_WIDTH_WIDE));
+        options.add(new OptionInstance<>("options.raised.displacement.y", OptionInstance.cachedConstantTooltip(Component.translatable("options.raised.displacement.y.tooltip")), (prefix, value) -> Options.genericValueLabel(prefix, value == 0 ? CommonComponents.OPTION_OFF : Component.literal(Configure.getDisplacementY(current.toString()) + "px (" + Math.round(Math.ceil((value.floatValue() / ((float) height / 4)) * 100)) + "%)")), new OptionInstance.IntRange(0, height / 4), Configure.getDisplacementY(current.toString()), value -> Configure.setDisplacementY(current.toString(), value)).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING + (WIDGET_HEIGHT + SPACING), WIDGET_WIDTH_WIDE));
+        options.add(new OptionInstance<>("options.raised.direction.x", value -> Tooltip.create(Component.translatable("options.raised.direction.x.tooltip")), OptionInstance.forOptionEnum(), new OptionInstance.Enum<>(Arrays.asList(Layer.Direction.X.values()), Codec.INT.xmap(Layer.Direction.X::byId, Layer.Direction.X::getId)), Layer.Direction.X.byName(Configure.getDirectionX(current.toString()).getSerializedName()), value -> Configure.setDirectionX(current.toString(), value)).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING + (WIDGET_HEIGHT + SPACING) * 2, WIDGET_WIDTH_WIDE));
+        options.add(new OptionInstance<>("options.raised.direction.y", value -> Tooltip.create(Component.translatable("options.raised.direction.y.tooltip")), OptionInstance.forOptionEnum(), new OptionInstance.Enum<>(Arrays.asList(Layer.Direction.Y.values()), Codec.INT.xmap(Layer.Direction.Y::byId, Layer.Direction.Y::getId)), Layer.Direction.Y.byName(Configure.getDirectionY(current.toString()).getSerializedName()), value -> Configure.setDirectionY(current.toString(), value)).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING + (WIDGET_HEIGHT + SPACING) * 3, WIDGET_WIDTH_WIDE));
+        options.add(new OptionInstance<>("options.raised.sync", value -> Tooltip.create(Component.translatable("options.raised.sync.tooltip", value.toString())), (prefix, value) -> Component.literal(Parse.parsePath(value)), new OptionInstance.Enum<>(LayerRegistry.LAYERS.keySet().stream().sorted(Comparator.comparing(ResourceLocation::toString)).toList(), ResourceLocation.CODEC), ResourceLocation.tryParse(Configure.getSync(current.toString())), value -> Configure.setSync(current.toString(), value.toString())).createButton(minecraft.options, PADDING, HEADER_HEIGHT + PADDING + (WIDGET_HEIGHT + SPACING) * 4, WIDGET_WIDTH_WIDE));
 
         options.forEach(this::addRenderableWidget);
     }
@@ -84,16 +73,17 @@ public class RaisedScreen extends Screen {
         controls.forEach(this::addRenderableWidget);
     }
 
-    public void addList() {
-        list = new LayerList(minecraft, PANEL_WIDTH, height - HEADER_HEIGHT, this);
-        list.setList();
+    public void addLayers() {
+        layers = new LayerList(minecraft, PANEL_WIDTH, height - HEADER_HEIGHT, this);
 
-        addRenderableWidget(list);
+        layers.setLayers();
+
+        addRenderableWidget(layers);
     }
 
     public void getPrevious() {
         Set<String> mods = new HashSet<>();
-        Layers.LAYERS.keySet().forEach(key -> mods.add(key.getNamespace()));
+        LayerRegistry.LAYERS.keySet().forEach(key -> mods.add(key.getNamespace()));
         List<String> keys = mods.stream().toList();
         int index = keys.indexOf(current.getNamespace()) - 1;
 
@@ -109,7 +99,7 @@ public class RaisedScreen extends Screen {
 
     public void getNext() {
         Set<String> mods = new HashSet<>();
-        Layers.LAYERS.keySet().forEach(key -> mods.add(key.getNamespace()));
+        LayerRegistry.LAYERS.keySet().forEach(key -> mods.add(key.getNamespace()));
         List<String> keys = mods.stream().toList();
         int index = keys.indexOf(current.getNamespace()) + 1;
 
@@ -124,10 +114,10 @@ public class RaisedScreen extends Screen {
     }
 
     public void setMod(String mod) {
-        RaisedScreen.current = Layers.LAYERS.keySet().stream().sorted(Comparator.comparing(ResourceLocation::toString)).filter(name -> name.getNamespace().equals(mod)).toList().get(0);
+        RaisedScreen.current = LayerRegistry.LAYERS.keySet().stream().sorted(Comparator.comparing(ResourceLocation::toString)).filter(name -> name.getNamespace().equals(mod)).toList().get(0);
 
         resetOptions();
-        resetList();
+        resetLayers();
         resetControls();
     }
 
@@ -143,22 +133,22 @@ public class RaisedScreen extends Screen {
         addControls();
     }
 
-    public void resetList() {
-        list.children().clear();
-        list.setList();
+    public void resetLayers() {
+        layers.children().clear();
+        layers.setLayers();
     }
 
     @Override
     public void repositionElements() {
-        list.updateSize(PANEL_WIDTH, height - HEADER_HEIGHT, HEADER_HEIGHT, height);
-        list.setLeftPos(width - PANEL_WIDTH);
+        layers.updateSize(PANEL_WIDTH, height - HEADER_HEIGHT, HEADER_HEIGHT, height);
+        layers.setLeftPos(width - PANEL_WIDTH);
     }
 
     @Override
     public void onClose() {
         super.onClose();
-        if (lastScreen != null) {
-            minecraft.setScreen(lastScreen);
+        if (parent != null) {
+            minecraft.setScreen(parent);
         }
     }
 
@@ -166,12 +156,12 @@ public class RaisedScreen extends Screen {
     public void resize(Minecraft minecraft, int width, int height) {
         super.resize(minecraft, width, height);
 
-        if (Option.getLayer(current.toString()) != null) {
-            if (Option.getDisplacementX(current.toString()) > width / 4) {
-                Option.setDisplacementX(current.toString(), width / 4);
+        if (Configure.getLayer(current.toString()) != null) {
+            if (Configure.getDisplacementX(current.toString()) > width / 4) {
+                Configure.setDisplacementX(current.toString(), width / 4);
             }
-            if (Option.getDisplacementY(current.toString()) > height / 4) {
-                Option.setDisplacementY(current.toString(), height / 4);
+            if (Configure.getDisplacementY(current.toString()) > height / 4) {
+                Configure.setDisplacementY(current.toString(), height / 4);
             }
         }
 
@@ -184,16 +174,11 @@ public class RaisedScreen extends Screen {
         renderBackground(guiGraphics);
         super.render(guiGraphics, mouseX, mouseY, partialTick);
 
-        guiGraphics.fill(0, 0, width, (PADDING + WIDGET_HEIGHT + PADDING), 1073741824);
+        guiGraphics.fill(0, 0, width, HEADER_HEIGHT, 1073741824);
 
         guiGraphics.drawCenteredString(font, title, PADDING + (WIDGET_WIDTH_WIDE / 2), PADDING + 6, -1);
 
         AbstractWidgetInvoker.invokeRenderScrollingString(guiGraphics, font, Component.literal(Parse.parseNamespace(current)), width - (PADDING + WIDGET_WIDTH_WIDE - WIDGET_WIDTH_SQUARE), PADDING, width - (PADDING + WIDGET_WIDTH_SQUARE), PADDING + WIDGET_HEIGHT, -1);
-
-        if (Option.getLayer(current.toString()) != null) {
-            displacementX.active = Option.getSync(current.toString()).equals(current.toString());
-            displacementY.active = Option.getSync(current.toString()).equals(current.toString());
-        }
     }
 
     @Override
--- src/main/java/dev/yurisuika/raised/mixin/client/gui/GuiMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/GuiMixin.java
@@ -1,8 +1,8 @@
 package dev.yurisuika.raised.mixin.client.gui;
 
-import dev.yurisuika.raised.client.gui.Layers;
+import dev.yurisuika.raised.client.gui.Layer;
+import dev.yurisuika.raised.registry.LayerRegistry;
 import dev.yurisuika.raised.util.Translate;
-import dev.yurisuika.raised.util.config.options.Layer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,59 +13,46 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public abstract class GuiMixin {
 
-    public abstract static class Hotbar {
+    @Mixin(value = Gui.class, priority = -999999999)
+    public abstract static class Pre {
 
-        @Mixin(value = Gui.class, priority = -999999999)
-        public abstract static class Pre {
+        /**
+         * Resizes the hotbar selector to draw the entire texture.
+         */
+        @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V", ordinal = 1), index = 6)
+        private int replaceHotbarSelectorHeight(int height) {
+            return 24;
+        }
 
-            /**
-             * Resizes the hotbar selector to draw the entire texture.
-             */
-            @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V", ordinal = 1), index = 6)
-            private int replaceHotbarSelectorHeight(int height) {
-                return 24;
-            }
+        /**
+         * Moves mod elements at the head/tail of the HUD render for {@link Layer} key "minecraft:other".
+         */
+        @Inject(method = "render", at = @At("HEAD"))
+        private void startRenderHeadTranslate(GuiGraphics guiGraphics, float partialTick, CallbackInfo ci) {
+            Translate.start(guiGraphics.pose(), LayerRegistry.OTHER);
+        }
 
+        @Inject(method = "render", at = @At("TAIL"))
+        private void startRenderTailTranslate(GuiGraphics guiGraphics, float partialTick, CallbackInfo ci) {
+            Translate.start(guiGraphics.pose(), LayerRegistry.OTHER);
         }
 
     }
 
-    public abstract static class Other {
+    @Mixin(value = Gui.class, priority = 999999999)
+    public abstract static class Post {
 
-        @Mixin(value = Gui.class, priority = -999999999)
-        public abstract static class Pre {
-
-            /**
-             * Moves mod elements at the head/tail of the HUD render for {@link Layer} key "minecraft:other".
-             */
-            @Inject(method = "render", at = @At("HEAD"))
-            private void startRenderHeadTranslate(GuiGraphics guiGraphics, float partialTick, CallbackInfo ci) {
-                Translate.start(guiGraphics.pose(), Layers.OTHER);
-            }
-
-            @Inject(method = "render", at = @At("TAIL"))
-            private void startRenderTailTranslate(GuiGraphics guiGraphics, float partialTick, CallbackInfo ci) {
-                Translate.start(guiGraphics.pose(), Layers.OTHER);
-            }
-
+        /**
+         * Moves mod elements at the head/tail of the HUD render for {@link Layer} key "minecraft:other".
+         */
+        @Inject(method = "render", at = @At("HEAD"))
+        private void endRenderHeadTranslate(GuiGraphics guiGraphics, float partialTick, CallbackInfo ci) {
+            Translate.end(guiGraphics.pose());
         }
 
-        @Mixin(value = Gui.class, priority = 999999999)
-        public abstract static class Post {
-
-            /**
-             * Moves mod elements at the head/tail of the HUD render for {@link Layer} key "minecraft:other".
-             */
-            @Inject(method = "render", at = @At("HEAD"))
-            private void endRenderHeadTranslate(GuiGraphics guiGraphics, float partialTick, CallbackInfo ci) {
-                Translate.end(guiGraphics.pose());
-            }
-
-            @Inject(method = "render", at = @At("TAIL"))
-            private void endRenderTailTranslate(GuiGraphics guiGraphics, float partialTick, CallbackInfo ci) {
-                Translate.end(guiGraphics.pose());
-            }
-
+        @Inject(method = "render", at = @At("TAIL"))
+        private void endRenderTailTranslate(GuiGraphics guiGraphics, float partialTick, CallbackInfo ci) {
+            Translate.end(guiGraphics.pose());
         }
 
     }
