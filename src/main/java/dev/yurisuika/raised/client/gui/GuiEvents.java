60daf1ed27adf32329d57c602ceb4b2357c048d1
yurisuika <yurisuika@protonmail.com>
2025-06-25 04:09:39 -0700

    Tidying up.

--- src/main/java/dev/yurisuika/raised/client/gui/GuiEvents.java
+++ src/main/java/dev/yurisuika/raised/client/gui/GuiEvents.java
@@ -1,7 +1,7 @@
 package dev.yurisuika.raised.client.gui;
 
+import dev.yurisuika.raised.registry.LayerRegistry;
 import dev.yurisuika.raised.util.Translate;
-import dev.yurisuika.raised.util.config.options.Layer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
@@ -24,7 +24,7 @@ public class GuiEvents {
     @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
     public void startTranslate(RenderGuiOverlayEvent.Pre event) {
         ResourceLocation name = formatOverlay(event.getOverlay().id());
-        if (name != null && Layers.LAYERS.containsKey(name)) {
+        if (name != null && LayerRegistry.LAYERS.containsKey(name)) {
             if (!translated) {
                 translated = true;
                 Translate.start(event.getGuiGraphics().pose(), name);
@@ -35,7 +35,7 @@ public class GuiEvents {
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
     public void endTranslate(RenderGuiOverlayEvent.Pre event) {
         ResourceLocation name = formatOverlay(event.getOverlay().id());
-        if (name != null && Layers.LAYERS.containsKey(name) && event.isCanceled()) {
+        if (name != null && LayerRegistry.LAYERS.containsKey(name) && event.isCanceled()) {
             if (translated) {
                 translated = false;
                 Translate.end(event.getGuiGraphics().pose());
@@ -46,7 +46,7 @@ public class GuiEvents {
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void endTranslate(RenderGuiOverlayEvent.Post event) {
         ResourceLocation name = formatOverlay(event.getOverlay().id());
-        if (name != null && Layers.LAYERS.containsKey(name)) {
+        if (name != null && LayerRegistry.LAYERS.containsKey(name)) {
             if (translated) {
                 translated = false;
                 Translate.end(event.getGuiGraphics().pose());
@@ -56,37 +56,37 @@ public class GuiEvents {
 
     public static ResourceLocation formatOverlay(ResourceLocation name) {
         if (name.equals(VanillaGuiOverlay.HOTBAR.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.PLAYER_HEALTH.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.ARMOR_LEVEL.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.FOOD_LEVEL.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.AIR_LEVEL.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.MOUNT_HEALTH.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.JUMP_BAR.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.EXPERIENCE_BAR.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.ITEM_NAME.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.RECORD_OVERLAY.id())) {
-            return Layers.HOTBAR;
+            return LayerRegistry.HOTBAR;
         } else if (name.equals(VanillaGuiOverlay.CHAT_PANEL.id())) {
-            return Layers.CHAT;
+            return LayerRegistry.CHAT;
         } else if (name.equals(VanillaGuiOverlay.BOSS_EVENT_PROGRESS.id())) {
-            return Layers.BOSSBAR;
+            return LayerRegistry.BOSSBAR;
         } else if (name.equals(VanillaGuiOverlay.SCOREBOARD.id())) {
-            return Layers.SIDEBAR;
+            return LayerRegistry.SIDEBAR;
         } else if (name.equals(VanillaGuiOverlay.POTION_ICONS.id())) {
-            return Layers.EFFECTS;
+            return LayerRegistry.EFFECTS;
         } else if (name.equals(VanillaGuiOverlay.PLAYER_LIST.id())) {
-            return Layers.PLAYERS;
+            return LayerRegistry.PLAYERS;
         } else if (name.equals(VanillaGuiOverlay.SUBTITLES.id())) {
-            return Layers.SUBTITLES;
+            return LayerRegistry.SUBTITLES;
         } else if (MODS.contains(name)) {
             return name;
         } else {
