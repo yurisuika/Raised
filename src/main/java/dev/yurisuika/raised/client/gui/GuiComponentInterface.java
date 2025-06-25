ba503f77c03fa3e46ce6871a222651b8da610e0a
yurisuika <yurisuika@protonmail.com>
2025-06-25 04:09:57 -0700

    Tidying up.

--- src/main/java/dev/yurisuika/raised/client/gui/GuiComponentInterface.java
+++ src/main/java/dev/yurisuika/raised/client/gui/GuiComponentInterface.java
@@ -1,10 +1,10 @@
-package dev.yurisuika.raised.client.gui.components;
+package dev.yurisuika.raised.client.gui;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
 
-public interface AbstractWidgetInterface {
+public interface GuiComponentInterface {
 
     void renderScrollingString(PoseStack poseStack, Font font, Component text, int minX, int minY, int maxX, int maxY, int color);
 
--- src/main/java/dev/yurisuika/raised/mixin/client/gui/components/toasts/ToastComponentMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/components/toasts/ToastComponentMixin.java
@@ -2,9 +2,9 @@ package dev.yurisuika.raised.mixin.client.gui.components.toasts;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import dev.yurisuika.raised.client.gui.Layers;
+import dev.yurisuika.raised.client.gui.Layer;
+import dev.yurisuika.raised.registry.LayerRegistry;
 import dev.yurisuika.raised.util.Translate;
-import dev.yurisuika.raised.util.config.options.Layer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,24 +12,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 public abstract class ToastComponentMixin {
 
-    public abstract static class Toasts {
+    @Mixin(targets = "net.minecraft.client.gui.components.toasts.ToastComponent$ToastInstance", priority = -999999999)
+    public abstract static class Pre {
 
-        @Mixin(targets = "net.minecraft.client.gui.components.toasts.ToastComponent$ToastInstance", priority = -999999999)
-        public abstract static class Pre {
+        /**
+         * Moves the {@code toasts} for {@link Layer} key "minecraft:toasts".
+         */
+        @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V"))
+        private void startToastsTranslate(int x, PoseStack poseStack, CallbackInfoReturnable<Boolean> cir) {
+            Translate.start(RenderSystem.getModelViewStack(), LayerRegistry.TOASTS);
+        }
 
-            /**
-             * Moves the {@code toasts} for {@link Layer} key "minecraft:toasts".
-             */
-            @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;pushPose()V"))
-            private void startToastsTranslate(int x, PoseStack poseStack, CallbackInfoReturnable<Boolean> cir) {
-                Translate.start(RenderSystem.getModelViewStack(), Layers.TOASTS);
-            }
-
-            @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V", shift = At.Shift.AFTER))
-            private void endToastsTranslate(int x, PoseStack poseStack, CallbackInfoReturnable<Boolean> cir) {
-                Translate.end(RenderSystem.getModelViewStack());
-            }
-
+        @Inject(method = "render", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V", shift = At.Shift.AFTER))
+        private void endToastsTranslate(int x, PoseStack poseStack, CallbackInfoReturnable<Boolean> cir) {
+            Translate.end(RenderSystem.getModelViewStack());
         }
 
     }
