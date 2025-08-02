cadf24a43dab07e576fa7a3f92c94e9a30c8e1fd
yurisuika <yurisuika@proton.me>
2025-08-02 08:51:34 -0700

    Move LayerList into RaisedScreen to support focus.

--- src/main/java/dev/yurisuika/raised/client/gui/screens/RaisedScreen.java
+++ src/main/java/dev/yurisuika/raised/client/gui/screens/RaisedScreen.java
@@ -4,7 +4,7 @@ import com.mojang.serialization.Codec;
 import dev.yurisuika.raised.client.RaisedOptions;
 import dev.yurisuika.raised.client.gui.Layer;
 import dev.yurisuika.raised.client.gui.Resource;
-import dev.yurisuika.raised.client.gui.components.LayerList;
+import dev.yurisuika.raised.client.gui.components.SpacedSelectionList;
 import dev.yurisuika.raised.mixin.client.gui.components.AbstractWidgetInvoker;
 import dev.yurisuika.raised.registry.LayerRegistry;
 import dev.yurisuika.raised.util.Configure;
@@ -17,11 +17,16 @@ import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.sounds.SoundEvents;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class RaisedScreen extends Screen {
 
@@ -76,7 +81,7 @@ public class RaisedScreen extends Screen {
     }
 
     public void addLayers() {
-        layers = new LayerList(minecraft, PANEL_WIDTH, height - HEADER_HEIGHT, this);
+        layers = new LayerList(minecraft, PANEL_WIDTH, height - HEADER_HEIGHT);
 
         layers.setLayers();
 
@@ -196,6 +201,61 @@ public class RaisedScreen extends Screen {
             return true;
         }
         return true;
+    }
+
+    public class LayerList extends SpacedSelectionList<LayerList.Entry> {
+
+        public LayerList(Minecraft minecraft, int width, int height) {
+            super(minecraft, width, height, 0, WIDGET_HEIGHT, PADDING);
+        }
+
+        public void setLayers() {
+            clearEntries();
+            LayerRegistry.LAYERS.keySet().stream().sorted(Comparator.comparing(ResourceLocation::toString)).filter(location -> location.getNamespace().equals(RaisedScreen.current.getNamespace())).forEach(name -> addEntry(new Entry(name)));
+        }
+
+        public class Entry extends SpacedSelectionList.Entry<Entry> {
+
+            public final ResourceLocation name;
+
+            public Entry(ResourceLocation name) {
+                this.name = name;
+                setCurrent(RaisedScreen.current.equals(name));
+            }
+
+            @Override
+            public void render(GuiGraphics guiGraphics, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean hovering, float partialTick) {
+                renderScrollingString(guiGraphics, Minecraft.getInstance().font, Component.literal(Parse.parsePath(name)), left + WIDGET_WIDTH_SQUARE, top, left + (WIDGET_WIDTH_WIDE - WIDGET_WIDTH_SQUARE), top + WIDGET_HEIGHT, -1);
+
+                AtomicReference<ResourceLocation> texture = new AtomicReference<>(ResourceLocation.fromNamespaceAndPath("raised", "textures/gui/layer/default.png"));
+                Minecraft.getInstance().getResourcePackRepository().openAllSelected().forEach(pack -> pack.listResources(PackType.CLIENT_RESOURCES, "raised", "textures/gui/layer/" + name.getNamespace() + "/" + name.getPath() + ".png", (location, supplier) -> texture.set(location)));
+                guiGraphics.blit(RenderPipelines.GUI_TEXTURED, texture.get(), left, top, 0, 0, WIDGET_WIDTH_SQUARE, WIDGET_HEIGHT, WIDGET_WIDTH_SQUARE, WIDGET_HEIGHT);
+
+                guiGraphics.blit(RenderPipelines.GUI_TEXTURED, ResourceLocation.fromNamespaceAndPath("raised", "textures/gui/direction/" + Configure.getDirectionX(name.toString()).toString().toLowerCase() + "_" + Configure.getDirectionY(name.toString()).toString().toLowerCase() + ".png"), left + (WIDGET_WIDTH_WIDE - WIDGET_WIDTH_SQUARE), top, 0, 0, WIDGET_WIDTH_SQUARE, WIDGET_HEIGHT, WIDGET_WIDTH_SQUARE, WIDGET_HEIGHT);
+            }
+
+            @Override
+            public Component getNarration() {
+                return Component.translatable("narrator.select", Parse.parsePath(name));
+            }
+
+            @Override
+            public boolean mouseClicked(double mouseX, double mouseY, int button, boolean bl) {
+                Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+                setCurrent(true);
+                return true;
+            }
+
+            public void setCurrent(boolean current) {
+                if (current) {
+                    RaisedScreen.current = name;
+                    resetOptions();
+                    LayerList.this.setSelected(this);
+                }
+            }
+
+        }
+
     }
 
 }
